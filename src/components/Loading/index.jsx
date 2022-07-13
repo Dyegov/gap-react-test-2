a07299c929d287929bfb910dbdad1a4e82738e72
Dyegov <diego.valencr@gmail.com>
2022-07-12 19:37:28 -0500

    Fixed Loading background-image since it was applying to the whole app and not only to the component

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -2,10 +2,12 @@ import './Loading.scss'
 
 const Loading = () => {
   return (
-    <div className='loading'>
-      <div className='text'>Loading awesome Pokemon for you...</div>
-      <img src='/Hourglass.gif' />
-      <img className='pikachu' src='/pikachu.png' />
+    <div className='wrapper'>
+      <div className='loading'>
+        <div className='text'>Loading awesome Pokemon for you...</div>
+        <img src='/Hourglass.gif' />
+        <img className='pikachu' src='/pikachu.png' />
+      </div>
     </div>
   )
 }
